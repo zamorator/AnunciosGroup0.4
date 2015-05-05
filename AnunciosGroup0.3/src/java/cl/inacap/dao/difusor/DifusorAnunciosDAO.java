fbc24fcc9075aeae24d4899d185dbab90f6e1baa
Rubenqs <ruben.quinteros89@gmail.com>
2015-05-04 23:25:27 -0400

    RQS: Se agrega procedimiento almacenado en DifusorAnunciosDAO.java.

--- AnunciosGroup0.3/src/java/cl/inacap/dao/difusor/DifusorAnunciosDAO.java
+++ AnunciosGroup0.3/src/java/cl/inacap/dao/difusor/DifusorAnunciosDAO.java
@@ -7,10 +7,12 @@ package cl.inacap.dao.difusor;
 
 import cl.inacap.connect.ConnectionFactory;
 import cl.inacap.model.Anuncio;
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.Types;
 import java.util.ArrayList;
 
 /**
@@ -18,45 +20,40 @@ import java.util.ArrayList;
  * @author macbookair
  */
 public class DifusorAnunciosDAO {
-    
-    
-    public ArrayList<Anuncio> buscaranuncio() throws Exception {
-        ConnectionFactory cf = new ConnectionFactory();
-        Connection con = null;
-        PreparedStatement pst = null;
-        StringBuilder query = null;
-        ResultSet rs = null;
-        int respuesta = 0;
-        ArrayList<Anuncio> anuncios = new ArrayList<Anuncio>();
-        System.out.println("Query anuncios inicio");
-        try {
+        
+        public ArrayList<Anuncio> buscaranuncio() throws Exception {
+            ConnectionFactory cf = new ConnectionFactory();
+            Connection con = null;
+            ResultSet rs = null;
+            ArrayList<Anuncio> anuncios = new ArrayList<Anuncio>();
+            System.out.println(con);  
+         try {  
             con = cf.obtenerConexion();
-            query = new StringBuilder();
-            query.append("SELECT `CODIGO_ANUNCIO`, `NOMBRE_U_ANUNCIANTE`, `NOMBRE_ANUNCIO`, `IMAGEN_ANUNCIO`, `DESCRIPCION_ANUNCIO` FROM `anuncio` WHERE `ESTADO_ANUNCIO` = 'V'");
-            System.out.println(query);
-            pst = con.prepareStatement(query.toString());
-            rs = pst.executeQuery();
+            // se crea instancia a procedimiento.
+            CallableStatement proc = con.prepareCall("{CALL SP_BUSCAR_ANUNCIO()}");
+             System.out.println(proc);
             
-            while (rs.next()) {
-                Anuncio anuncio = new Anuncio();
-                anuncio.setCodigo_anuncio(rs.getInt("CODIGO_ANUNCIO"));
-                anuncio.setNombre_u_anunciante(rs.getString("NOMBRE_U_ANUNCIANTE"));
-                anuncio.setNombre_anuncio(rs.getString("NOMBRE_ANUNCIO"));
-                anuncio.setImagen_anuncio(rs.getString("IMAGEN_ANUNCIO"));
-                anuncio.setDescripcion_anuncio(rs.getString("DESCRIPCION_ANUNCIO"));
-                anuncios.add(anuncio);
-                
+            rs = proc.executeQuery();
+            
+            while(rs.next()){
+            Anuncio anuncio = new Anuncio();
+            anuncio.setCodigo_anuncio(rs.getInt("CODIGO_ANUNCIO"));
+            anuncio.setNombre_u_anunciante(rs.getString("NOMBRE_U_ANUNCIANTE"));
+            anuncio.setNombre_anuncio(rs.getString("NOMBRE_ANUNCIO"));
+            anuncio.setImagen_anuncio(rs.getString("IMAGEN_ANUNCIO"));
+            anuncio.setDescripcion_anuncio(rs.getString("DESCRIPCION_ANUNCIO"));
+            anuncios.add(anuncio);
             }
-            System.out.println(anuncios.size());
-        } catch (Exception ex) {
+        }catch (Exception ex) {                  
+            System.out.println(ex);
             ex.printStackTrace();
             throw new Exception();
-        } finally {
+       }finally {
             con = null;
-            query = null;
             cf = null;
-        }
-        return anuncios;
-    }
+        } 
+         return anuncios;
+
+         }  
 }
 
